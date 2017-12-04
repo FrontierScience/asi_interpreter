9d28e7153b426ad957545dbffb552509c46932a7
kylelambert5 <34169296+kylelambert5@users.noreply.github.com>
2017-12-04 10:20:13 -0500

    Add DTD and ASI Exception files

---
+++ src/java/org/fstrf/stanfordAsiInterpreter/resistance/ASIParsingException.java
@@ -0,0 +1,49 @@
+/**
+Copyright 2017 Frontier Science & Technology Research Foundation
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+ADDITIONAL DISCLAIMER:
+In addition to the standard warranty exclusions and limitations of 
+liability set forth in sections 7, 8 and 9 of the Apache 2.0 license 
+that governs the use and development of this software, Frontier Science 
+& Technology Research Foundation disclaims any liability for use of 
+this software for patient care or in clinical settings. This software 
+was developed solely for use in medical and public health research, and 
+was not intended, designed, or validated to guide patient care.
+*/ 
+
+
+
+package org.fstrf.stanfordAsiInterpreter.resistance;
+
+public class ASIParsingException extends Exception {
+
+	private static final long serialVersionUID = 8708340139375769665L;
+
+	public ASIParsingException() {
+		super();
+	}
+	
+	public ASIParsingException(String message) {
+		super(message);
+	}
+	
+	public ASIParsingException(String message, Throwable cause)  {
+		super(message, cause);
+	}
+	
+	public ASIParsingException(Throwable cause) {
+		super(cause);
+	}
+}
